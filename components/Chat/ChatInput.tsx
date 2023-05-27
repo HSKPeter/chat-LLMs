a7657b9d1a4e90043ed1199c013828dcdc685b12
Peter Hui <huishikuen@gmail.com>
2023-05-27 15:49:18 -0700

    Set focus ring style for prompt input field

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -313,7 +313,7 @@ export const ChatInput = ({
 
           <textarea
             ref={textareaRef}
-            className="m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 pl-10 text-black dark:bg-transparent dark:text-white md:py-3 md:pl-10"
+            className="m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 pl-10 text-black dark:bg-transparent dark:text-white md:py-3 md:pl-10 focus:outline-none focus:ring focus:border-blue-500"
             style={{
               resize: 'none',
               bottom: `${textareaRef?.current?.scrollHeight}px`,
