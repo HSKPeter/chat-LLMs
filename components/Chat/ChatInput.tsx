2dcd0c87a203d5671c4314b154a9a5661c53d8b1
Peter Hui <huishikuen@gmail.com>
2023-05-29 11:16:58 +0800

    Add border radius outline for textarea

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -383,7 +383,7 @@ export const ChatInput = ({
           </button>}
           <textarea
             ref={textareaRef}
-            className={`m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 text-black dark:bg-transparent dark:text-white md:py-3 focus:outline-none focus:ring focus:border-blue-500 ${toEnablePromptOptimizationFeature ? "pl-10 md:pl-10" : "pl-4 md:pl-4"}`}
+            className={`m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 text-black dark:bg-transparent dark:text-white md:py-3 focus:outline-none focus:ring focus:border-blue-500 rounded-md ${toEnablePromptOptimizationFeature ? "pl-10 md:pl-10" : "pl-4 md:pl-4"}`}
             style={{
               resize: 'none',
               bottom: `${textareaRef?.current?.scrollHeight}px`,
