0448d2f548342ee951dcdc862fefa1c3549d2719
Peter Hui <huishikuen@gmail.com>
2023-05-29 11:19:27 +0800

    Replace IconMessage with ModelLabel

--- components/Chatbar/components/Conversation.tsx
+++ components/Chatbar/components/Conversation.tsx
@@ -105,7 +105,7 @@ export const ConversationComponent = ({ conversation }: Props) => {
     <div className="relative flex items-center">
       {isRenaming && selectedConversation?.id === conversation.id ? (
         <div className="flex w-full items-center gap-3 rounded-lg bg-[#343541]/90 p-3">
-          <IconMessage size={18} />
+          <ModelLabel model={selectedConversation?.model} />
           <input
             className="mr-12 flex-1 overflow-hidden overflow-ellipsis border-neutral-400 bg-transparent text-left text-[12.5px] leading-3 text-white outline-none focus:border-neutral-100"
             type="text"
