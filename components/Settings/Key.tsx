0460e987c182f51090a21584b6dd3673744ef982
Peter Hui <huishikuen@gmail.com>
2023-05-29 18:07:12 +0800

    Change hover style

--- components/Settings/Key.tsx
+++ components/Settings/Key.tsx
@@ -11,14 +11,12 @@ interface Props {
 export const Key: FC<Props> = ({ name, apiKey, onApiKeyChange }) => {
   const { t } = useTranslation('sidebar');
   const [isVisible, setIsVisible] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
   <div className="duration:200 flex w-full cursor-pointer items-center rounded-md py-3 px-3 transition-colors">
     <div className="mr-2 text-black dark:text-neutral-200">{name}</div>
 
     <input
-      ref={inputRef}
       className="ml-2 h-[20px] flex-1 overflow-hidden overflow-ellipsis border-b border-neutral-400 bg-transparent pr-1 text-[12.5px] leading-3 text-left text-black dark:text-neutral-200 outline-none focus:border-neutral-600 dark:focus:border-neutral-100"
       type={isVisible ? 'text' : 'password'}
       value={apiKey}
@@ -30,12 +28,12 @@ export const Key: FC<Props> = ({ name, apiKey, onApiKeyChange }) => {
       {
         isVisible 
         ? <IconEye
-            className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
+            className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-600 dark:hover:text-neutral-100"
             size={18}
             onClick={(e) => setIsVisible(!isVisible)}
           />
         : <IconEyeOff
-            className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
+            className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-600 dark:hover:text-neutral-100"
             size={18}
             onClick={(e) => setIsVisible(!isVisible)}
           />
