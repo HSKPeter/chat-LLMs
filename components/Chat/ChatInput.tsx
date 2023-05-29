f4cc18a91b0bdb1ed3f8a54d1c86b6ba3192567f
Peter Hui <huishikuen@gmail.com>
2023-05-29 11:25:07 +0800

    Update footer text

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -453,7 +453,7 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/HSKPeter/chat-LLMs"
           target="_blank"
           rel="noreferrer"
           className="underline"
@@ -462,7 +462,7 @@ export const ChatInput = ({
         </a>
         .{' '}
         {t(
-          "Chat LLMs is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Chat LLMs is an advanced chatbot kit for different large language models, aiming to mimic ChatGPT's interface and functionality.",
         )}
       </div>
     </div>
